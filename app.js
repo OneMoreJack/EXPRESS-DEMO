1f328f4fd2f2158637fcd79160c1f83317a078fc
JackLiR8 <jiakeli1024@gmail.com>
2020-06-29 14:26:10 +0800

    app级别的中间件

--- app.js
+++ app.js
@@ -26,6 +26,30 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/birds', birdsRouter)
 
+/*
+  To skip the rest of the middleware functions from a router
+  middleware stack, call next('route') to pass control to the 
+  next route.
+
+  NOTE: next('route') will work only in middleware functions that 
+        were loaded by using the app.METHOD() or router.METHOD() 
+        functions.
+*/
+app.get('/user/:id', function (req, res, next) {
+  // if the user ID is 0, skip to the next route
+  if (req.params.id === '0') next('route')
+  // otherwise pass the control to the next middleware function in this stack
+  else next()
+}, function (req, res, next) {
+  // send a regular response
+  res.send('regular')
+})
+
+// handler for the /user/:id path, which sends a special response
+app.get('/user/:id', function (req, res, next) {
+  res.send('special')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
